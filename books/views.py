3204e030d3535aeb875ef8faaee66db5ffed713e
unknown <pouriashiralipour@gmail.com>
2022-04-08 16:53:52 +0430

    add update and create for image

--- books/views.py
+++ books/views.py
@@ -18,13 +18,13 @@ class BookDetailView(generic.DetailView):
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title', 'author', 'content', 'price']
+    fields = ['title', 'author', 'content', 'price', 'cover', ]
     template_name = 'books/book_create.html'
 
 
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title', 'content', 'price']
+    fields = ['title', 'author', 'content', 'price', 'cover']
     template_name = 'books/book_update.html'
 
 
