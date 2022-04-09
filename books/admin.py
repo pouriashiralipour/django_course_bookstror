3853633d1c7557ff9ae2b6683232f567dfc19717
unknown <pouriashiralipour@gmail.com>
2022-04-09 12:21:14 +0430

    add commentform to books app

--- books/admin.py
+++ books/admin.py
@@ -4,7 +4,7 @@ from .models import Book, Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'book', 'text', 'datetime_created', )
+    list_display = ('user', 'book', 'text', 'datetime_created', 'recommended', 'is_active', )
 
 
 class BookAdmin(admin.ModelAdmin):
