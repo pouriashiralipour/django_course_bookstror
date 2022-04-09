101a9c56728eb927bd62713e320fe98c19c6e346
unknown <pouriashiralipour@gmail.com>
2022-04-09 09:21:13 +0430

    add comments model to books app

--- books/admin.py
+++ books/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
 
 
-# @admin.register(Book)
-# class PostAdmin(admin.ModelAdmin):
-#     pass
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'datetime_created', )
 
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', )
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Book, BookAdmin)
+
+
 
